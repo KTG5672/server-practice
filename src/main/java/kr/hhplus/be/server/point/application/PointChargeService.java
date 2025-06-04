f8e61dcc50ed6dabae195fb3cfcb84d1a9d330c7
KTG5672 <zzang5992@gmail.com>
2025-06-05 00:07:35 +0900

    refactor: 포인트 충전 서비스 주석 추가 - 클래스, 메서드 상세 주석 작성

--- src/main/java/kr/hhplus/be/server/point/application/PointChargeService.java
+++ src/main/java/kr/hhplus/be/server/point/application/PointChargeService.java
@@ -6,6 +6,9 @@ import kr.hhplus.be.server.user.domain.repository.UserRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+/**
+ * 유저의 포인트 충전 로직을 수행하는 비지니스 서비스
+ */
 @Service
 public class PointChargeService {
 
@@ -15,6 +18,14 @@ public class PointChargeService {
         this.userRepository = userRepository;
     }
 
+    /**
+     * 포인트 충전 메서드
+     * - 유저 ID로 유저를 조회하고, 존재하지 않으면 UserNotFoundException 발생시킨다.
+     * - 유저 도메인의 chargePoint 메서드를 호출하여 포인트를 충전한다.
+     * - 변경된 유저 정보를 저장한다.
+     * @param userId 유저 ID
+     * @param chargePoint 충전 포인트
+     */
     @Transactional
     public void chargePoint(String userId, long chargePoint) {
         User user = userRepository.findById(userId).orElseThrow(() ->
