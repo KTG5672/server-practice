cec68467087c28592056dd38513488fd41d519eb
KTG5672 <zzang5992@gmail.com>
2025-06-08 21:00:50 +0900

    feat: 포인트 사용 서비스 구현 및 테스트 추가 - PointUseService.usePoint 메서드 구현 - 관련 단위 테스트 작성

---
+++ src/main/java/kr/hhplus/be/server/point/application/PointUseService.java
@@ -0,0 +1,44 @@
+package kr.hhplus.be.server.point.application;
+
+import kr.hhplus.be.server.point.domain.model.PointTransactionHistory;
+import kr.hhplus.be.server.point.domain.model.TransactionType;
+import kr.hhplus.be.server.point.domain.repository.PointTransactionHistoryRepository;
+import kr.hhplus.be.server.user.domain.exception.UserNotFoundException;
+import kr.hhplus.be.server.user.domain.model.User;
+import kr.hhplus.be.server.user.domain.repository.UserRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class PointUseService {
+
+    private final UserRepository userRepository;
+    private final PointTransactionHistoryRepository pointTransactionHistoryRepository;
+
+    public PointUseService(UserRepository userRepository,
+        PointTransactionHistoryRepository pointTransactionHistoryRepository) {
+        this.userRepository = userRepository;
+        this.pointTransactionHistoryRepository = pointTransactionHistoryRepository;
+    }
+
+    /**
+     * 포인트 사용 메서드
+     * - 유저 ID로 유저를 조회하고, 존재하지 않으면 UserNotFoundException 발생시킨다.
+     * - 유저 도메인의 usePoint 메서드를 호출하여 포인트를 사용한다.
+     * - 변경된 유저 정보를 저장한다.
+     * - 사용 내역을 저장한다.
+     * @param userId 유저 ID
+     * @param usePoint 사용 포인트
+     */
+    @Transactional
+    public void usePoint(String userId, long usePoint) {
+        User user = userRepository.findById(userId).orElseThrow(() ->
+            new UserNotFoundException(userId));
+        user.usePoint(usePoint);
+        userRepository.save(user);
+
+        PointTransactionHistory pointTransactionHistory = PointTransactionHistory.of(userId,
+            usePoint, TransactionType.USE);
+        pointTransactionHistoryRepository.save(pointTransactionHistory);
+    }
+}
