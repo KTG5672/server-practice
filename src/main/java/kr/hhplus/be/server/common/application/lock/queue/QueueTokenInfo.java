5207323205096adc7b0a5d6d544308f87b8433b8
KTG5672 <zzang5992@gmail.com>
2025-06-12 22:29:57 +0900

    feat: Redis 기반 대기열 토큰 관리 기능 구현 - RedisQueueTokenManager 클래스 구현 - 토큰 발급: ZSet 및 Key-Value 저장 - 토큰 정보 조회: 순위 계산 및 상태 반환 - 토큰 제거: ZSet 및 Key 삭제

--- src/main/java/kr/hhplus/be/server/common/application/lock/queue/QueueTokenInfo.java
+++ src/main/java/kr/hhplus/be/server/common/application/lock/queue/QueueTokenInfo.java
@@ -1,5 +1,5 @@
 package kr.hhplus.be.server.common.application.lock.queue;
 
-public record QueueTokenInfo(String token, long rank, long totalWait, QueueTokenStatus status) {
+public record QueueTokenInfo(String token, Long rank, Long totalWaitMinute, QueueTokenStatus status) {
 
 }
