5e4b384756744b03febacf4d918d5ab1d603a2f3
KTG5672 <zzang5992@gmail.com>
2025-06-25 00:30:39 +0900

    feat: Redis 캐시 설정 및 의존성 추가 - spring-boot-starter-cache 의존성 추가 - CacheConfig 클래스 및 RedisCacheConfiguration Bean 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,6 +36,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-data-redis")
 	implementation("org.springframework.retry:spring-retry")
+	implementation("org.springframework.boot:spring-boot-starter-cache")
 
 	compileOnly("org.projectlombok:lombok")
 	annotationProcessor("org.projectlombok:lombok")
