c9d7484b8e40885d188b99e7fd4372c8d3ad4c61
KTG5672 <zzang5992@gmail.com>
2025-06-26 00:45:25 +0900

    feat: 테스트 환경 RedissonConfig 제외 및 SpEL 기반 분산락 키 지원 - 테스트 환경에서 RedissonConfig 제외하여 테스트 컨테이너 충돌 방지 - 분산락 AOP 에서 SpEL 기반 메서드 파라미터를 사용한 락 키 해석 기능 추가

--- src/main/java/kr/hhplus/be/server/common/lock/infrastructure/DistributedLockAspect.java
+++ src/main/java/kr/hhplus/be/server/common/lock/infrastructure/DistributedLockAspect.java
@@ -5,8 +5,13 @@ import kr.hhplus.be.server.common.lock.infrastructure.exception.TryLockFailedExc
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
+import org.springframework.expression.EvaluationContext;
+import org.springframework.expression.ExpressionParser;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
+import org.springframework.expression.spel.support.StandardEvaluationContext;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.support.TransactionSynchronization;
 import org.springframework.transaction.support.TransactionSynchronizationManager;
@@ -24,7 +29,8 @@ public class DistributedLockAspect {
 
     @Around(value = "@annotation(distributedLock)")
     public Object around(ProceedingJoinPoint joinPoint, DistributedLock distributedLock) throws Throwable {
-        String lockKey = distributedLock.key();
+
+        String lockKey = getLockKey(joinPoint, distributedLock);
         long waitTime = distributedLock.waitTime();
         long leaseTime = distributedLock.leaseTime();
         TimeUnit timeUnit = distributedLock.timeUnit();
@@ -53,6 +59,25 @@ public class DistributedLockAspect {
         }
 
         return joinPoint.proceed();
+    }
+
+    private String getLockKey(ProceedingJoinPoint joinPoint,
+        DistributedLock distributedLock) {
+        MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
+
+        // 1. SpEL 표현식 파서를 생성
+        ExpressionParser parser = new SpelExpressionParser();
+        // 2. SpEL이 값을 조회할 때 사용할 컨텍스트(변수 저장소) 생성
+        EvaluationContext context = new StandardEvaluationContext();
+        // 3. 현재 AOP 타겟 메서드의 파라미터 이름과 값들을 꺼냄
+        String[] paramNames = methodSignature.getParameterNames();
+        // 4. 각 파라미터 이름에 대해 context 값 매핑
+        Object[] args = joinPoint.getArgs();
+        for (int i = 0; i < paramNames.length; i++) {
+            context.setVariable(paramNames[i], args[i]);
+        }
+        // 5. 파싱하여 문자열로 반환
+        return parser.parseExpression(distributedLock.key()).getValue(context, String.class);
     }
 
     private TransactionSynchronization getSynchronization(RLock lock) {
