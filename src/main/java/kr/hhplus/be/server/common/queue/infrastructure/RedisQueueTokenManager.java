52abd3b8e6bb1155f5308981968ccd3105876eb9
KTG5672 <zzang5992@gmail.com>
2025-07-04 01:50:26 +0900

    feat: 대기열 큐에서 활성 큐로 이동 기능 구현 및 스케줄러 추가 - QueueTokenManager.moveWaitQueueToActiveQueue 구현 - Lua script 사용하여 원자성 보장 - 1초마다 실행 되는 스케줄러 추가 - 관련 테스트 코드 작성

--- src/main/java/kr/hhplus/be/server/common/queue/infrastructure/RedisQueueTokenManager.java
+++ src/main/java/kr/hhplus/be/server/common/queue/infrastructure/RedisQueueTokenManager.java
@@ -1,12 +1,14 @@
 package kr.hhplus.be.server.common.queue.infrastructure;
 
 import java.time.Duration;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 import kr.hhplus.be.server.common.queue.application.QueueTokenInfo;
 import kr.hhplus.be.server.common.queue.application.QueueTokenManager;
 import kr.hhplus.be.server.common.queue.application.QueueTokenStatus;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
 import org.springframework.stereotype.Component;
 
 /**
@@ -14,6 +16,7 @@ import org.springframework.stereotype.Component;
  * - 토큰 발급 기능 (대기열 등록, 토큰 정보 반환)
  * - 토큰 정보 조회 기능
  * - 대기열에서 토큰 제거 기능
+ * - 대기열 Queue에서 활성 Queue로 이동 기능
  */
 @Component
 public class RedisQueueTokenManager implements QueueTokenManager {
@@ -21,8 +24,29 @@ public class RedisQueueTokenManager implements QueueTokenManager {
     private static final long ACTIVE_LIMIT = 10_000;
     private static final String TOKEN_KEY_PREFIX = "queue:token:";
     private static final String WAIT_QUEUE_KEY = "wait_queue";
+    private static final String ACTIVE_QUEUE_KEY = "active_queue";
 
     private final RedisTemplate<String, String> redisTemplate;
+
+    private static final DefaultRedisScript<List<?>> QUEUE_MOVE_SCRIPT;
+
+    // Lua script 객체 로딩
+    static {
+        String script =
+            "local activeSize = redis.call('SCARD', KEYS[2]) " +
+                "if activeSize >= tonumber(ARGV[1]) then return {} end " +
+                "local moveSize = tonumber(ARGV[1]) - activeSize " +
+                "local tokens = redis.call('ZRANGE', KEYS[1], 0, moveSize - 1) " +
+                "if #tokens == 0 then return {} end " +
+                "for i = 1, #tokens do " +
+                "  redis.call('ZREM', KEYS[1], tokens[i]) " +
+                "  redis.call('SADD', KEYS[2], tokens[i]) " +
+                "end " +
+                "return tokens";
+
+        QUEUE_MOVE_SCRIPT = new DefaultRedisScript<>();
+        QUEUE_MOVE_SCRIPT.setScriptText(script);
+    }
 
     public RedisQueueTokenManager(RedisTemplate<String, String> redisTemplate) {
         this.redisTemplate = redisTemplate;
@@ -55,33 +79,46 @@ public class RedisQueueTokenManager implements QueueTokenManager {
     public QueueTokenInfo getTokenInfo(String token) {
         Boolean exists = Optional.of(redisTemplate.hasKey(TOKEN_KEY_PREFIX + token)).orElse(false);
         if (!exists) {
-            // 만료 되었다고 판단하고 대기열 토큰 제거하고 만료 응답
             leaveQueue(token);
             return new QueueTokenInfo(token, null, null, QueueTokenStatus.EXPIRED);
         }
+
+        Boolean isActive = redisTemplate.opsForSet().isMember(ACTIVE_QUEUE_KEY, token);
+        if (Boolean.TRUE.equals(isActive)) {
+            return new QueueTokenInfo(token, null, null, QueueTokenStatus.ACTIVE);
+        }
+
         Long waitCount = redisTemplate.opsForZSet().rank(WAIT_QUEUE_KEY, token);
         if (waitCount == null) {
-            // 토큰이 정상적으로 저장되지 않았다면 제거 후 만료 응답
             leaveQueue(token);
             return new QueueTokenInfo(token, null, null, QueueTokenStatus.EXPIRED);
         }
-        QueueTokenStatus status =
-            waitCount > ACTIVE_LIMIT ? QueueTokenStatus.WAITING : QueueTokenStatus.ACTIVE;
-        long totalWaitMinute = 0;
-        if (status == QueueTokenStatus.WAITING) {
-            // 총 대기시간(분) = (총 대기열 숫자 - 활성화 유저) x 2
-            totalWaitMinute = (waitCount - ACTIVE_LIMIT) * 2;
-        }
-        return new QueueTokenInfo(token, waitCount, totalWaitMinute, status);
+
+        long totalWaitMinute = (waitCount + 1) * 2;
+        return new QueueTokenInfo(token, waitCount, totalWaitMinute, QueueTokenStatus.WAITING);
     }
 
     /**
-     * 대기열 큐 및 Key-Value 에서 토큰 정보 제거
+     * 대기열/활성 Queue 및 Key-Value 에서 토큰 정보 제거
      * @param token 대기열 토큰
      */
     @Override
     public void leaveQueue(String token) {
         redisTemplate.opsForZSet().remove(WAIT_QUEUE_KEY, token);
+        redisTemplate.opsForSet().remove(ACTIVE_QUEUE_KEY, token);
         redisTemplate.delete(TOKEN_KEY_PREFIX + token);
+    }
+
+    /**
+     * 대기열 Queue에서 활성 Queue로 이동
+     * - 원자성을 위하여 lua script 적용
+     * - 활성화 Queue의 남은 자리 만큼 한번에 이동 (최대 활성화 수 - 현재 활성화 수)
+     */
+    public void moveWaitQueueToActiveQueue() {
+        redisTemplate.execute(
+            QUEUE_MOVE_SCRIPT,
+            List.of(WAIT_QUEUE_KEY, ACTIVE_QUEUE_KEY),
+            String.valueOf(ACTIVE_LIMIT)
+        );
     }
 }
