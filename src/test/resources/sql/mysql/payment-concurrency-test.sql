b032600f296c0444444350997c931769fbeda948
KTG5672 <zzang5992@gmail.com>
2025-07-07 23:26:53 +0900

    fix: 테스트 깨짐 수정 - 콘서트-일정-좌석을 직접 연관관계로 수정하여 테스트 SQL 수정 - 대기열 토큰 방식(Waiting Queue -> Active Queue)이 바뀌어 통합 테스트 흐름 변경

--- src/test/resources/sql/mysql/payment-concurrency-test.sql
+++ src/test/resources/sql/mysql/payment-concurrency-test.sql
@@ -1,8 +1,10 @@
 INSERT INTO users (id, email, password, point)
 VALUES ('payment-test-user-1', 'payment-test1@email.com', '1234', 10000);
 
+INSERT INTO concerts (id, name) VALUES (9001, 'test-concert');
+
 INSERT INTO concert_schedules (id, concert_id, place, start_date_time, ticket_open_date_time)
-VALUES (9001, 1, 'test-place', ADDDATE(NOW(), INTERVAL 10 DAY), ADDDATE(NOW(), INTERVAL -1 DAY));
+VALUES (9001, 9001, 'test-place', ADDDATE(NOW(), INTERVAL 10 DAY), ADDDATE(NOW(), INTERVAL -1 DAY));
 
 INSERT INTO concert_seats (id, schedule_id, zone, no, price)
 VALUES (9001, 9001, 'A', 1, 10000);
@@ -11,7 +13,7 @@ INSERT INTO users (id, email, password, point)
 VALUES ('payment-test-user-2', 'payment-test2@email.com', '1234', 10000);
 
 INSERT INTO concert_schedules (id, concert_id, place, start_date_time, ticket_open_date_time)
-VALUES (9002, 1, 'test-place', ADDDATE(NOW(), INTERVAL 10 DAY), ADDDATE(NOW(), INTERVAL -1 DAY));
+VALUES (9002, 9001, 'test-place', ADDDATE(NOW(), INTERVAL 10 DAY), ADDDATE(NOW(), INTERVAL -1 DAY));
 
 INSERT INTO concert_seats (id, schedule_id, zone, no, price)
 VALUES (9002, 9002, 'A', 1, 10000);
@@ -20,7 +22,7 @@ INSERT INTO users (id, email, password, point)
 VALUES ('payment-test-user-3', 'payment-test3@email.com', '1234', 5000);
 
 INSERT INTO concert_schedules (id, concert_id, place, start_date_time, ticket_open_date_time)
-VALUES (9003, 1, 'test-place', ADDDATE(NOW(), INTERVAL 10 DAY), ADDDATE(NOW(), INTERVAL -1 DAY));
+VALUES (9003, 9001, 'test-place', ADDDATE(NOW(), INTERVAL 10 DAY), ADDDATE(NOW(), INTERVAL -1 DAY));
 
 INSERT INTO concert_seats (id, schedule_id, zone, no, price)
 VALUES (9003, 9003, 'A', 1, 10000);
